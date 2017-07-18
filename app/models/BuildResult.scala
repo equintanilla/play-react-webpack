854141c38aba76cf6efab0c41838e51d17b62dd9
yogesh <shardul@us.ibm.com>
2017-07-18 12:12:48 +0530

    Adding Unit test for build result REST API

--- app/models/BuildResult.scala
+++ app/models/BuildResult.scala
@@ -2,7 +2,7 @@ package models
 
 import org.joda.time.DateTime
 
-import play.api.libs.json.{ Json, Reads, Writes }
+import play.api.libs.json.{ Json, Reads, Writes, Format }
 
 import play.api.libs.functional.syntax.{ unapply, unlift }
 import play.api.libs.json.OWrites
@@ -11,8 +11,10 @@ case class BuildResult(tag: String, last_commit: String, jenkins_url: String, da
 
 
 object BuildResult{
+  
   implicit val jodaDateReads = Reads.jodaDateReads("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
   implicit val jodaDateWrites = Writes.jodaDateWrites("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+  implicit val jodaDateTimeFormats = Format(jodaDateReads, jodaDateWrites)
   
   implicit val buildResultReads: Reads[BuildResult] = Json.reads[BuildResult] /*(
     (JsPath \ "tag").read[String] and
--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -4,6 +4,7 @@ import org.junit.runner._
 
 import play.api.test._
 import play.api.test.Helpers._
+import play.api.libs.json.{ Json }
 
 /**
  * Add your spec here.
@@ -25,6 +26,24 @@ class ApplicationSpec extends Specification {
       status(home) must equalTo(OK)
       contentType(home) must beSome.which(_ == "text/html")
       contentAsString(home) must contain ("CI performance dashboard")
+    }
+    
+    "test build result REST API with POST" in new WithApplication{
+      val home = route(FakeRequest(POST, "/api/build_info").withJsonBody(Json.parse("""{"tag":"v2.2.0-rc4","last_commit":"9fd064be5ab9f07f4eb768e45ae82c0b03c00137","jenkins_url":"https://sys-aix-jenkins.swg-devops.com:8443/job/ART-SPARK-PERF-TEST/job/Matrix_test_full/","date":"2017-04-20T08:25:26.807Z"}"""))).get
+
+      status(home) must equalTo(OK)
+      contentType(home) must beSome.which(_ == "text/plain")
+      println(contentAsString(home))
+      contentAsString(home) must contain ("inserted")
+    }
+    
+    "test build result REST API with GET" in new WithApplication{
+      val home = route(FakeRequest(GET, "/api/build_info")).get
+
+      status(home) must equalTo(OK)
+      contentType(home) must beSome.which(_ == "application/json")
+      println(contentAsString(home))
+      contentAsString(home) must contain ("jenkins_url")
     }
   }
 }
